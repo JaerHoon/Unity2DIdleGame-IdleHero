cd2ce6d06502f6d5aad96420293c34874ea9dac7
yangstar98 <167849650+yangstar98@users.noreply.github.com>
2024-04-24 16:01:40 +0900

    vagante리소스 추가 정리 및 폴더 추가 폰트 위치 정리
    00

---
+++ Assets/_01Scripts/Monsters/MonsterData.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "MonsterDate", menuName ="Scriptable Object/Monster Data", order = int.MaxValue)]
+
+
+public class MonsterData : ScriptableObject
+{
+    [SerializeField]
+    private string MonsterName;
+    public string monsterName { get { return MonsterName; } }
+
+    [SerializeField]
+    private int Hp;
+    public int hp { get { return hp; } }
+
+    [SerializeField]
+    private int Damage;
+    public int damage { get { return Damage; } }
+
+    [SerializeField]
+    private int Defense;
+    public int defense { get { return Defense; } }
+
+    [SerializeField]
+    private int MoveSpeed;
+    public int moveSpeed { get { return MoveSpeed; } }
+}
---
+++ Assets/_01Scripts/Monsters/MonsterFactory.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterFactory
+{//몬스터 재활용 스크립트
+
+    List<MonsterRecycle> pool = new List<MonsterRecycle>();//몬스터 풀 리스트 저장
+    MonsterRecycle monsterPrefab;//몬스터 프리팹
+    int defaultMonsterNumber;//처음 pool에 만들 몬스터 수
+
+    //프리팹 정보 강제 주입
+    public MonsterFactory(MonsterRecycle monsterPrefab, int defaultMonsterNumber = 5)
+    {
+        this.monsterPrefab = monsterPrefab;//외부에서 받은 프리팹 정보 
+        this.defaultMonsterNumber = defaultMonsterNumber;//외부에서 받은 몬스터 수 설정
+        Debug.Assert(this.monsterPrefab != null, "몬스터 팩토리에 몬스터 프리팹 없음");
+    }
+
+
+    //오브젝트 생성
+    void CreatePool()
+    {
+        for (int i = 0; i < defaultMonsterNumber; i++)
+        {//비활성으로 생성할 몬스터 생성
+            MonsterRecycle obj = GameObject.Instantiate(monsterPrefab) as MonsterRecycle;
+            obj.gameObject.SetActive(false);//생성 후 바로 비활성화
+            pool.Add(obj);//pool 리스트에 저장
+        }
+    }
+
+    //몬스터 불러오기
+    public MonsterRecycle GetMonster()
+    {
+        if(pool.Count == 0) CreatePool();//pool에 남아있는 몬스터가 없다면 몬스터 생성
+        int lastIndex = pool.Count - 1;// pool리스트의 마지막 인덱스
+        MonsterRecycle obj = pool[lastIndex];//pool리스트 마지막 몬스터를 obj에 담음
+        pool.RemoveAt(lastIndex);//비활성 상태의 리스트 마지막 몬스터를 리스트에서 제거(사용 했으면 리스트에서 제거하기 위해)
+        obj.gameObject.SetActive(true);//사용을 위해 몬스터 활성화
+        return obj;//몬스터 
+ 
+
+       
+    }
+
+    //반납함수 재활용하기 위해
+    public void MonsterRestore(MonsterRecycle obj)
+    {
+        Debug.Assert(obj != null, "아무것도 없는 오브젝트는 반환되어야 합니다");
+        obj.gameObject.SetActive(false);//상용된 몬스터 비활성화
+        pool.Add(obj);//재활용하기 위해 다시 pool에 추가
+    }
+}
