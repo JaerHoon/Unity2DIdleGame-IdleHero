749e2fa1d49b962c713d54f04a2c3111e82870af
JaerHoon <159917860+JaerHoon@users.noreply.github.com>
2024-04-25 14:28:20 +0900

    아이템 뽑기 스크립트

---
+++ Assets/_01Scripts/UI/Item_Summons/ItemSlot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ItemSlot : MonoBehaviour
+{
+    [SerializeField]
+    Image itemIcon;
+    [SerializeField]
+    Image backGround;
+    [SerializeField]
+    Image slot;
+    [SerializeField]
+    TextMeshProUGUI itemName;
+
+    public void Setting(Item item)
+    {
+        itemIcon.sprite = item.itemData.icon;
+        backGround.sprite = item.backGround;
+        slot.sprite = item.slot;
+        itemName.text = item.itemData.koreanName;
+    }
+}
