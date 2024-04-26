70309a55beb3c75dec3c97de004321e58c6bfcab
HyunJun Sung <157206691+shade-light@users.noreply.github.com>
2024-04-26 16:25:46 +0900

    0426 플레이어 공격 성공
    공격 구현 어느정도 성공

--- Assets/_01Scripts/Player/PlayerAttack.cs
+++ Assets/_01Scripts/Player/PlayerAttack.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    Transform playerTr;
-    public Transform monsterTr;
+    Transform playerTr; // 플레이어 위치
+    public Transform pos; // 공격 인식할 위치
+    public Transform DownPos;
     Animator anim;
     Rigidbody2D rb;
+    Transform minDisMon;
 
+    public LayerMask layermask;
+    float range = 1.0f;
+    int monsterLayer;
     
 
-
-    
     float attackDistance = 3.0f;
-    
-    //bool isAttack = false;
+    public bool isAttack = false;
+    public bool isMove;
+
+    float scaleX;
+    float scaleY;
+    float scaleZ;
+
+    [SerializeField]
+    Vector2 boxSize;
     void Start()
     {
         playerTr = GameObject.FindWithTag("Player").GetComponent<Transform>();
@@ -24,43 +34,110 @@ public class PlayerAttack : MonoBehaviour
         //pos = GameObject.Find("pos").GetComponentInChildren<Transform>();
         anim = GetComponent<Animator>();
 
-        anim.SetBool("attack", false);
+        monsterLayer = LayerMask.NameToLayer("monster");
+        layermask = 1 << monsterLayer;
+
+        scaleX = transform.localScale.x;
+        scaleY = transform.localScale.y;
+        scaleZ = transform.localScale.z;
 
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void IsMonsterAttack()
     {
-        if (collision.gameObject.tag == "monster")
+        //Vector2 plusPos = new Vector2(pos.position.x + 1.3f, pos.position.y + 1.3f);
+        //Vector2 size = new Vector2(1.2f, 2.0f);
+        Collider2D[] AttackArea = Physics2D.OverlapBoxAll(pos.position, boxSize, 0);
+
+        foreach (Collider2D colls in AttackArea)
         {
-            anim.SetInteger("attack", 1);
+            if(colls.CompareTag("monster"))
+            {
+                isAttack = true;
+                attackAnim();
+            }
+            
+        }
+      
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(pos.position, range);
+
+        
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(pos.position, boxSize);
+    }
+
+
+    public void IsMonsterRecognition()
+    {
+        Collider2D[] RecognitionArea = Physics2D.OverlapCircleAll(playerTr.position, range, layermask, -100.0f,100.0f);
+        
+
+        if (RecognitionArea.Length > 0)
+        {
+
+            float distance = float.MaxValue; // float의 최댓값
+            for (int i = 0; i < RecognitionArea.Length; i++)
+            {
+                if (distance > Vector2.Distance(playerTr.position,RecognitionArea[i].transform.position))
+                {
+                    distance = Vector2.Distance(playerTr.position,RecognitionArea[i].transform.position);
+                    minDisMon = RecognitionArea[i].transform;
+                    
+                }
+            }
+           
+            
+            if(isMove==false)
+            {
+                if (minDisMon.position.x > playerTr.position.x)
+                    transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+                else
+                    transform.localScale = new Vector3(-scaleX, scaleY, scaleZ);
+
+
+                
+            }
+
+            minDisMon = null;
+            distance = 10000;
+            
+           
+
+
         }
        
-
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+   
+    void attackAnim()
     {
-        if (collision.gameObject.tag == "monster")
-        {
-            anim.SetInteger("attack", 0);
-        }
-
-        
+        anim.SetInteger("attack", 1); // 공격 애니메이션 동작
     }
 
-
-    /*public void ActiveSkill(Skill_ScriptableObject skill)
+    void stopAttackAnim()
     {
-        
-        anim.Play(skill.animationName);
-    }*/
-
-
-    // Update is called once per frame
+        anim.SetInteger("attack", 0); // 공격 애니메이션 멈춤
+    }
+   
     void Update()
     {
        
+        IsMonsterRecognition();
+
     }
 
-  
+    private void FixedUpdate() // 정해진 시간 간격으로 호출됨 => Physics2D 쓰는 함수 계산할 때 용이함.
+    {
+        if (isMove == true) // 움직일 때 공격모션 멈추고 run 애니메이션으로 돌아가기
+        {
+            stopAttackAnim();
+            return;
+        }
+        IsMonsterAttack();
+    }
+
 }
