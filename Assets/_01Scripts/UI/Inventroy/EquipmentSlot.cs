1fc4ebe1bf7e98e6e954a49d8c0b37bcd9030d94
KimJaeHoon <kmandm@naver.com>
2024-04-30 18:00:10 +0900

    UI

---
+++ Assets/_01Scripts/UI/Inventroy/EquipmentSlot.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentSlot : Slots
+{
+    public enum SlotType { Weapon, Shield, Helmet, Armor, Shoes, Accessories }
+    public SlotType slotType;
+
+    [SerializeField]
+    EquipmentItem equipmentItem;
+
+    Item equitedItem;
+
+    private void OnEnable()
+    {
+        EquipItem(equitedItem);
+    }
+
+    void NullSetting()
+    {
+        itemIcon.sprite = ItemManager.instance.defaultItemIcon[(int)slotType];
+        itemIcon.color = Color.black;
+        backGround.sprite = ItemManager.instance.defaultBackGround;
+        frame.sprite = ItemManager.instance.defaultSlot;
+
+        if (itemName != null) itemName.text = null;
+        if (levelText != null) levelText.text = null;
+        if (itemGrade != null) itemGrade.text = null;
+        if (itemPow != null) itemPow.text = null;
+
+    }
+
+    public void EquipItem(Item item)
+    {
+        if(item != null)
+        {
+            equitedItem = item;
+            itemIcon.color = Color.white;
+            Setting(item);
+        }
+        else
+        {
+            NullSetting();
+        }
+    }
+
+    public Item GetItem()
+    {
+        return equitedItem;
+    }
+    
+    
+}
