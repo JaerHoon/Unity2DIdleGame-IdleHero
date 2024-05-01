b0370cbe7048f22651611921e0e8bcfbf25e8e3a
HyunJun Sung <157206691+shade-light@users.noreply.github.com>
2024-05-01 14:57:59 +0900

    0501 오전 저장
    오전 저장

--- Assets/_01Scripts/Skills/Wind.cs
+++ Assets/_01Scripts/Skills/Wind.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class Wind : MonoBehaviour
 {
-    
+    public Ease ease;
     void Start()
     {
-        
+        transform.DOScale(5.0f, 1.5f).SetEase(ease);
     }
 
     // Update is called once per frame
@@ -15,6 +15,6 @@ public class Wind : MonoBehaviour
     {
 
         transform.Translate(Vector2.right * 5.0f * Time.deltaTime);
-        Destroy(gameObject, 1.5f);
+        //Destroy(gameObject, 1.5f);
     }
 }
