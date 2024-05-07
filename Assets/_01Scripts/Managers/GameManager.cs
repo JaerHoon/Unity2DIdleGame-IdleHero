2d06f30cfbe7a09ffd5a014d140462b6ed4dea41
yangstar98 <167849650+yangstar98@users.noreply.github.com>
2024-05-07 16:12:48 +0900

    일시정지 BGM 소리 조절

--- Assets/_01Scripts/Managers/GameManager.cs
+++ Assets/_01Scripts/Managers/GameManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField] GameObject pausePanel;//일시정지 패널 프리팹
+    private bool isPause = false;
 
     public static GameManager instance;//스폰 매니저 싱글톤 생성
 
@@ -17,6 +21,9 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this);
         }
+
+
+        pausePanel.SetActive(false);//일시정시 패널 비활성화
     }
 
     public PlayerAttack playerAttack;
@@ -25,6 +32,13 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         playerAttack = GameObject.FindWithTag("Player").GetComponent<PlayerAttack>();
+        //SoundManager.instance.PlaySound(SoundId.BGM);
+    }
+
+    public void OnSetPausePanel()
+    {
+        isPause = !isPause;
+        pausePanel.SetActive(isPause);
     }
 
     // Update is called once per frame
--- Assets/_01Scripts/Managers/SoundManager.cs
+++ Assets/_01Scripts/Managers/SoundManager.cs
@@ -1,13 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager instance;
+
+    AudioSource myAudioSource;
+    public AudioMixer audioMixer;
+    //public AudioMixer audioMixer_BGM;
+    //public AudioMixer audioMixer_Effect;
+
+    public Slider audioSlider_Master;
+    public Slider audioSlider_BGM;
+    public Slider audioSlider_Effect;
+
+    [SerializeField]
+    AudioStorage soundStorage;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+
+    public void AudioMasterControl()
+    {
+        float volume_Master = audioSlider_Master.value;
+        if(volume_Master == -40f) audioMixer.SetFloat("Master", -80f);
+        audioMixer.SetFloat("Master", volume_Master);
+        
+    }
+
+    public void AudioBGMControl()
+    {
+        float volume_BGM = audioSlider_BGM.value;
+        if (volume_BGM == -40f) audioMixer.SetFloat("BGM", -80f);
+        audioMixer.SetFloat("BGM", volume_BGM);
+    }
+
+    public void AudioEffectControl()
+    {
+        float volume_Effect = audioSlider_Effect.value;
+        if (volume_Effect == -40f) audioMixer.SetFloat("Effect", -80f);
+        audioMixer.SetFloat("Effect", volume_Effect);
+    }
+
+    int SoundChangeNum = 1;
+
+    public void OnChangeAudioClip()
+    {
+        myAudioSource.Stop();
+        myAudioSource.clip = soundStorage.SoundSrc[SoundChangeNum % soundStorage.SoundSrc.Length].SoundFile;
+        myAudioSource.Play();
+        SoundChangeNum++;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
