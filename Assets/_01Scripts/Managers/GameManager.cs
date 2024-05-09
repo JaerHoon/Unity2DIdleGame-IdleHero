7e5bdd220cd8b14f45ef190bf782e2b34dc45343
yangstar98 <167849650+yangstar98@users.noreply.github.com>
2024-05-09 17:50:08 +0900

    소리 설정 들어가기 버튼, 브금 체인지 버튼

--- Assets/_01Scripts/Managers/GameManager.cs
+++ Assets/_01Scripts/Managers/GameManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField] GameObject pausePanel;//일시정지 패널 프리팹
+    private bool isPause = false;
 
     public static GameManager instance;//스폰 매니저 싱글톤 생성
 
@@ -25,6 +29,12 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         playerAttack = GameObject.FindWithTag("Player").GetComponent<PlayerAttack>();
+    }
+
+    public void OnSetPausePanel()
+    {
+        isPause = !isPause;
+        pausePanel.SetActive(isPause);
     }
 
     // Update is called once per frame
