f156275ecf9914c632a39526a55c866975849599
KimJaeHoon <kmandm@naver.com>
2024-05-10 17:15:40 +0900

    게임메지너

--- Assets/_01Scripts/Managers/GameManager.cs
+++ Assets/_01Scripts/Managers/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,19 +10,41 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject pausePanel;//일시정지 패널 프리팹
     private bool isPause = false;
 
-    public static GameManager instance;//스폰 매니저 싱글톤 생성
+    private static GameManager _instance;
+    // 인스턴스에 접근하기 위한 프로퍼티
+    public static GameManager Instance
+    {
+        get
+        {
+            // 인스턴스가 없는 경우에 접근하려 하면 인스턴스를 할당해준다.
+            if (!_instance)
+            {
+                _instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+
+                if (_instance == null)
+                    Debug.Log("no Singleton obj");
+            }
+            return _instance;
+        }
+    }
 
     private void Awake()
     {
-        if (instance == null)
+        if (_instance == null)
         {
-            instance = this;
+            _instance = this;
         }
-        else
+        // 인스턴스가 존재하는 경우 새로생기는 인스턴스를 삭제한다.
+        else if (_instance != this)
         {
-            Destroy(this);
+            Destroy(gameObject);
         }
+        // 아래의 함수를 사용하여 씬이 전환되더라도 선언되었던 인스턴스가 파괴되지 않는다.
+        DontDestroyOnLoad(gameObject);
     }
+    
+
+
 
     //public PlayerAttack playerAttack;
 
@@ -37,9 +60,9 @@ public class GameManager : MonoBehaviour
         pausePanel.SetActive(isPause);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeScene()
     {
-        
+
     }
+    
 }
