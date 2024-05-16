92a49f1456cac63cbffdfed8df022d2c414f2ace
yangstar98 <167849650+yangstar98@users.noreply.github.com>
2024-05-16 12:06:22 +0900

    구름효과

---
+++ Assets/_01Scripts/UI/Title/Cloud.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cloud : MonoBehaviour
+{
+
+    public Material cloud;
+
+    private void Start()
+    {
+        cloud = gameObject.GetComponent<Image>().material;
+    }
+
+    void Update()
+    {
+
+        float newOffSetX = cloud.mainTextureOffset.x + -0.1f * Time.deltaTime;
+        Vector2 newOffset = new Vector2(newOffSetX, 0);
+        cloud.mainTextureOffset = newOffset;
+
+
+
+    }
+}
